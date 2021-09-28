e8d30dc260a44179a55b837983ce1468b1b8f15c
Maribella <maribellavmoreno@gmail.com>
2021-09-28 19:07:23 -0300

    Comienzo de proyecto

---
+++ js/funcionalidad.js
@@ -0,0 +1,6 @@
+btn_enviar_registro.addEventListener("mouseover", function(){
+	this.style.backgroundColor = "darkslategray";});
+
+btn_enviar_registro.addEventListener("mouseleave", function(){
+	this.style.backgroundColor = "lightseagreen"
+});
